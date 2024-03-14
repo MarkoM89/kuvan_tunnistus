04be34aa2c400fe9e36bdad1ccc7c5ba3e055a78
Marko Mustonen <marko.mustonen3@metropolia.fi>
2024-03-14 15:07:55 +0200

    Pohja projektille

---
+++ src/kuvatunnistus.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+from ultralytics.models.yolo.detect import DetectionPredictor
+import cv2
+
+# Load a model
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+source = 'rtsp://192.168.0.10:8554/mjpeg/1'
+
+# Use the model
+results = model.predict(source, show = True)
+
+print(results)
