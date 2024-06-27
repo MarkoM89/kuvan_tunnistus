e822cbed739cd10e4c083dd8869c7757b1b22d24
Marko Mustonen <marko.mustonen3@metropolia.fi>
2024-06-27 17:55:36 +0300

    Päivitys viimeisimpään muokkaukseen.

--- src/kuvatunnistus.py
+++ src/kuvatunnistus.py
@@ -5,7 +5,7 @@ import cv2
 # Load a model
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
-source = 'rtsp://192.168.0.10:8554/mjpeg/1'
+source = 'rtsp://192.168.0.12:8554/mjpeg/1'
 
 # Use the model
 results = model.predict(source, show = True)
--- src/lintujen_tunnistus.py
+++ src/lintujen_tunnistus.py
@@ -3,7 +3,7 @@ from IPython.display import Image  # for displaying images
 import os 
 import random
 import shutil
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import _split
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 from tqdm import tqdm
